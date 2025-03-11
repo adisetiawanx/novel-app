1cee7f444323631f624f2a7b14e4f1cf2c78a07b
Kadek Adi Setiawan <adisetiawanzx@gmail.com>
2025-03-11 19:03:33 +0800

    rename the function and folder to make them more sense

--- internal/app/module/user_module.go
+++ internal/app/module/user_module.go
@@ -3,7 +3,7 @@ package module
 import (
 	"github.com/adisetiawanx/novel-app/internal/controller"
 	"github.com/adisetiawanx/novel-app/internal/repository"
-	"github.com/adisetiawanx/novel-app/internal/router"
+	"github.com/adisetiawanx/novel-app/internal/routes"
 	"github.com/adisetiawanx/novel-app/internal/service"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
@@ -14,5 +14,5 @@ func RegisterUserModule(server *echo.Echo, db *gorm.DB) {
 	userService := service.NewUserService(userRepository)
 	userController := controller.NewUserController(userService)
 
-	router.NewUserRouter(server, userController)
+	routes.RegisterUserRoutes(server, userController)
 }
--- internal/routes/user_router.go
+++ internal/routes/user_router.go
@@ -1,11 +1,11 @@
-package router
+package routes
 
 import (
 	"github.com/adisetiawanx/novel-app/internal/controller"
 	"github.com/labstack/echo/v4"
 )
 
-func NewUserRouter(server *echo.Echo, controller controller.UserController) {
+func RegisterUserRoutes(server *echo.Echo, controller controller.UserController) {
 	group := server.Group("/api")
 	group.POST("/user", controller.Create)
 }
